7669086caa644e64ae4a70c9c761ecc21df43248
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-07 20:49:17 +0400

    Test passed

--- lib/funbox_test.rb
+++ lib/funbox_test.rb
@@ -1,11 +1,12 @@
 require_relative 'funbox_test/version'
 require_relative 'funbox_test/reader'
 require_relative 'funbox_test/numbers'
+require 'pry'
 
 module FunboxTest
   def self.nmax(n)
     text = FunboxTest::Reader.text
     numbers = FunboxTest::Numbers.parse_numbers text
-    puts FunboxTest::Numbers.n_sorted_numbers numbers, ARGV[0].to_i
+    FunboxTest::Numbers.n_sorted_numbers numbers, n
   end
 end
